7a6395d2b7417e08fb2a9c2c873419201c3ed7b3
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-07-26 22:38:06 +0200

    finish iPhone Model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -5,13 +5,14 @@ import IPhone from "./IPhone";
 import * as THREE from "three";
 
 import { Suspense } from "react";
+import Loader from "./Loader";
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
   return (
     <View
     index={index}
     id={gsapType}
-    className={` w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
+    className={` w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''} `}>
 
       <ambientLight intensity={0.3} />
 
@@ -29,7 +30,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
       onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}/>
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0,0,0]}>
-      <Suspense fallback={<Html><div>Loading</div></Html>}>
+      <Suspense fallback={<Loader/>}>
         <IPhone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
         item={item}
         size={size}
