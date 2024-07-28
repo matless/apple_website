c92fa37cd0e8a72388c925b6b7cea6b2d554d5b3
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-07-28 21:07:13 +0200

    add Features

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 //...
-import * as Sentry from "@sentry/react";
+import * as Sentry from '@sentry/react';
 
 Sentry.init({
   dsn: "https://805c4f9ca98c098f07984397f30e300d@o4507448791334912.ingest.de.sentry.io/4507675933868112",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
     Sentry.reactRouterV6BrowserTracingIntegration({
       useEffect: React.useEffect,
     }),
-    Sentry.replayIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
   tracesSampleRate: 1.0, 
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  
-  replaysSessionSampleRate: 0.1,  
-  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
 });
 
 
